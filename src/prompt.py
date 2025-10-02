68af19130a32a95b143a3d471f9b0d7fe47a6eb0
pankaj pipely <pankajpipley82@gmail.com>
2025-10-02 17:47:49 +0530

    Added project files and updates

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+# System prompt
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following retrieved context to answer the question. "
+    "If you don't know the answer, say 'I don't know'. "
+    "Keep your answer concise, max 3 sentences."
+    "\n\n"
+    "{context}"
+)
